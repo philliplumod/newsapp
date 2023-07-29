6762edac8c40de8fc22df07ed897a3ec778c96fb
Phillip Jhon Lumod <44081207+philliplumod@users.noreply.github.com>
2023-07-29 15:26:58 +0800

    https://youtu.be/aaGcER1uUoE

--- lib/presentation/widgets/blog_widget.dart
+++ lib/presentation/widgets/blog_widget.dart
@@ -1,25 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 class BlogTile extends StatelessWidget {
-  final String imageUrl;
+  final String? imageUrl;
   final String title;
   final String desc;
   const BlogTile(
-      {super.key,
-      required this.imageUrl,
-      required this.title,
-      required this.desc});
+      {super.key, this.imageUrl, required this.title, required this.desc});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Column(
-        children: [
-          Image.network(imageUrl),
-          Text(title),
-          Text(desc)
-        ],
-      ),
+    return Column(
+      children: [
+        CachedNetworkImage(
+          imageUrl: imageUrl!,
+          placeholder: (context, url) => const CircularProgressIndicator(),
+          errorWidget: (context, url, error) => const Icon(Icons.error),
+        ),
+        Text(
+          title,
+          style: const TextStyle(fontSize: 17),
+        ),
+        Text(desc),
+      ],
     );
   }
 }
